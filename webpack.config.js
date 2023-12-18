906f7c0796b43ff488df6a12ae192cf3d4951d7a
hans <hans.mou@feibit.com>
2023-12-18 18:21:48 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+
+module.exports = {
+  // 这个属性有三种取值，分别是 production、development 和 none。
+  // 1. 生产模式下，Webpack 会自动优化打包结果；
+  // 2. 开发模式下，Webpack 会自动优化打包速度，添加一些调试过程中的辅助；
+  // 3. None 模式下，Webpack 就是运行最原始的打包，不做任何额外处理；
+  mode: "none",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    // path: path.join(__dirname, 'output'),
+    publicPath: "dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      //   {
+      //     test: /.png$/,
+      //     use: 'file-loader'
+      //   },
+      {
+        test: /.png$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 10 * 1024, // 10 KB 小文件使用base64 大文件使用url
+          },
+        },
+      },
+
+      {
+        test: /.html$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: ['img:src', 'a:href']
+          }
+        }
+      }
+    ],
+  },
+};
